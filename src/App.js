d25b467de2b7668b3730ecafceae445145aa1f1f
Erden777 <erden.aidynuly.99@gmail.com>
2021-02-23 01:36:18 +0600

    Initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <>Hello suka</>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import { Container } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Home(props){
+    return (
+        <></>
+    );
+}
+export default Home;
